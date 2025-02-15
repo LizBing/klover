4f4ab087052ec4c38ddb5cd32f7655c112a4f407
Lei <leizaakjyu@163.com>
2025-02-15 20:28:25 +0800

    add memory mapping and protection functions in os module

--- src/runtime/os.rs
+++ src/runtime/os.rs
@@ -23,9 +23,39 @@
 
 use std::sync::atomic::{AtomicI32, Ordering};
 
-use nix::libc::{sysconf, _SC_PAGE_SIZE};
+use nix::libc::{madvise, mmap, mprotect, sysconf, MADV_FREE, MAP_ANON, MAP_FAILED, MAP_FIXED, MAP_PRIVATE, PROT_EXEC, PROT_NONE, PROT_READ, PROT_WRITE, _SC_PAGE_SIZE};
 
-use crate::memory::mem_region::MemRegion;
+use crate::{memory::mem_region::MemRegion, util::global_defs::address};
+
+pub fn memmap(addr: address, size: usize) -> Option<MemRegion> {
+    let mut flags = MAP_ANON | MAP_PRIVATE;
+    if addr != 0 { flags |= MAP_FIXED; }
+
+    let res = unsafe {
+        mmap(addr as *mut _, size, PROT_NONE, flags, -1, 0)
+    };
+
+    if res == MAP_FAILED { return None; }
+
+    Some(MemRegion::with_size(res as _, size))
+}
+
+pub fn memprot(mr: MemRegion, accessible: bool, executable: bool) -> bool {
+    let mut prots = PROT_NONE;
+    if accessible { prots = PROT_READ | PROT_WRITE; }
+    if executable { prots |= PROT_EXEC; }
+
+    let res = unsafe {
+        mprotect(mr.begin() as _, mr.size(), prots)
+    };
+
+    return res == 0;
+}
+
+pub fn memadv_free(mr: MemRegion) -> bool {
+    let res = unsafe { madvise(mr.begin() as _, mr.size(), MADV_FREE) };
+    return res == 0;
+}
 
 pub fn get_page_size() -> usize {
     unsafe { sysconf(_SC_PAGE_SIZE) as _ }
