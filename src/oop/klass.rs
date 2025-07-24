08cd2c76a517848babd068882761f82d7590ff90
Lei <leizaakjyu@163.com>
2025-07-24 23:07:59 +0800

    update copyright

--- src/oop/klass.rs
+++ src/oop/klass.rs
@@ -1,25 +1,26 @@
 /*
- * Copyright (c) 2025, Lei Zaakjyu. All rights reserved.
+ * Copyright 2025 Lei Zaakjyu
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-pub struct Klass {}
+use crate::oop::obj_handle::ObjHandle;
+
+pub struct Klass {
+    _super: *mut Klass,
+    _mirror: ObjHandle,
+    _define_loader: ObjHandle,  // Nullable, which refers to Bootstrap Loader.
+}
 
 impl Klass {
     pub fn size_of_instance() -> usize {
